3d537bc33f2c4be44853e5dbe65ddea1a18a257a
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-07-02 21:56:11 +0900

    update env

--- apps/jobs/src/utils/env.ts
+++ apps/jobs/src/utils/env.ts
@@ -13,6 +13,10 @@ const envSchema = z.object({
   // RLS DISABLED
   DB_USER_RLS_DISABLED: z.string(),
   DB_PASS_RLS_DISABLED: z.string(),
+  // GITHUB TOKEN
+  // TODO: EnvからPERSONAL_ACCESS_TOKENを利用している箇所をDBから読み込むように修正(KMSで暗号化する)
+  // https://github.com/users/ToyB0x/projects/1/views/1?pane=issue&itemId=32241358
+  GITHUB_PERSONAL_ACCESS_TOKEN: z.string(),
 
   // OPTIONAL
   SENTRY_DSN: z.string().optional(),
