669caec70225c071cc58d8c6eff892e8768eaecb
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-07-02 16:58:49 +0900

    update env

--- apps/jobs/src/utils/env.ts
+++ apps/jobs/src/utils/env.ts
@@ -2,6 +2,12 @@ import { z } from 'zod'
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']),
+  GCP_PROJECT_ID: z.enum([
+    'gdash-test',
+    'gdash-dev',
+    'gdash-stg',
+    'gdash-prd',
+  ]),
 })
 
 export const getEnv = () => envSchema.parse(process.env)
