c9c1669b1e117511d7494af618a8810518618f43
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-07-02 19:01:00 +0900

    refactor

--- apps/jobs/src/commands/migrate/deploy.ts
+++ apps/jobs/src/commands/migrate/deploy.ts
@@ -1,42 +1,8 @@
 import { execSync } from 'child_process'
 import { getEnv } from '../../utils'
+import {execWithScrubbedLog, getDbUrl} from "./lib";
 
-export const deploy = async (): Promise<void> => {
-  const { DB_NAME,DB_USER, DB_PASS  } = getEnv()
-  const dbURL = `postgresql://${DB_USER}:${DB_PASS}@127.0.0.1:5432/${DB_NAME}?schema=public&connection_limit=1`
-
-  try {
-    const stdout = execSync(
-      `DATABASE_URL="${dbURL}" yarn workspace @g-dash/database prisma migrate deploy`
-    )
-    console.log(stdout.toString())
-    console.log('Migration completed successfully.')
-  } catch (e: any & { stdout: Buffer; stderr: Buffer }) {
-    // NOTE: hide DATABASE_URL on error for security reasons
-    if (
-      e.stdout.toString().includes('DATABASE_URL') ||
-      e.stdout.toString().includes('postgresql://')
-    ) {
-      console.info(
-        "Log is masked because this error message includes 'DATABASE_URL' or 'postgresql://'.",
-        '**********'
-      )
-    } else {
-      console.info(e.stdout.toString())
-    }
-
-    if (
-      e.stderr.toString().includes('DATABASE_URL') ||
-      e.stderr.toString().includes('postgresql://')
-    ) {
-      console.error(
-        "Log is masked because this error message includes 'DATABASE_URL' or 'postgresql://'.",
-        '**********'
-      )
-    } else {
-      console.error(e.stderr.toString())
-    }
-
-    process.exit(1)
-  }
+export const deploy = () => {
+  const dbURL = getDbUrl({...getEnv()})
+  execWithScrubbedLog(execSync, `DATABASE_URL="${dbURL}" yarn workspace @g-dash/database prisma migrate deploy`)
 }
