1343f490768db33734591816b9b19b376072c548
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-07-02 17:16:50 +0900

    update index / env

--- apps/jobs/src/utils/env.ts
+++ apps/jobs/src/utils/env.ts
@@ -3,6 +3,9 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'production', 'test']),
   SENTRY_DSN: z.string(),
+  DB_NAME: z.string(),
+  DB_USER: z.string(),
+  DB_PASS: z.string(),
   GCP_PROJECT_ID: z.enum([
     'gdash-test',
     'gdash-dev',
