aaf267d0d85e68ba4fa9a430bfe3fb950579b523
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-07-05 20:14:28 +0900

    move dir / rename

--- apps/jobs/src/commands/aggregate/aggregateByOrganization/aggregateByOrganization.ts
+++ apps/jobs/src/commands/aggregate/aggregateByOrganization/aggregateByOrganization.ts
@@ -1,7 +1,9 @@
 import { getSingleTenantPrismaClient } from '../../../utils'
 import { aggregateRepositories } from './aggregateRepositories'
 
-export const aggregateOrganization = async (orgName: string): Promise<void> => {
+export const aggregateByOrganization = async (
+  orgName: string
+): Promise<void> => {
   // TODO: orgIdを利用してRLSを有効化(/apps/jobs)
   // https://github.com/users/ToyB0x/projects/1/views/1?pane=issue&itemId=32240414
   const prismaSingleTenantClient = getSingleTenantPrismaClient()
