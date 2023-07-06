6bb1829e47ce96169a14d5574fa299be8fa9ecd8
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-07-07 06:19:11 +0900

    add theme

---
+++ apps/web/src/theme.ts
@@ -0,0 +1,17 @@
+import { extendTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  colors: {
+    brand: {
+      50: '#f1fdfd',
+      200: '#C4F1F9',
+      300: '#9DECF9',
+      400: '#76E4F7',
+      500: '#0BC5EA',
+      600: '#00B5D8',
+      700: '#00A3C4',
+      800: '#0987A0',
+      900: '#086F83',
+    },
+  },
+})
