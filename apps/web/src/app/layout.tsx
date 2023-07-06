6d3b70664501c9135a8ddd583e1753ea32a19b8d
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-07-07 06:07:46 +0900

    update root layout

---
+++ apps/web/src/app/layout.tsx
@@ -0,0 +1,15 @@
+import { Providers } from './providers'
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html>
+      <body>
+        <Providers>{children}</Providers>
+      </body>
+    </html>
+  )
+}
