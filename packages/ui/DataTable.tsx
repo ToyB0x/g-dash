05658b8390b10b804faa9c5a73ef1ffb1da0aaf2
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-07-07 17:58:16 +0900

    add DataTable

---
+++ packages/ui/DataTable.tsx
@@ -0,0 +1,91 @@
+'use client'
+import 'client-only'
+
+import { useState } from 'react'
+import { Table, Thead, Tbody, Tr, Th, Td, chakra } from '@chakra-ui/react'
+import { TriangleDownIcon, TriangleUpIcon, UpDownIcon } from '@chakra-ui/icons'
+import {
+  useReactTable,
+  flexRender,
+  getCoreRowModel,
+  ColumnDef,
+  SortingState,
+  getSortedRowModel,
+} from '@tanstack/react-table'
+
+export type DataTableProps<Data extends object> = {
+  data: Data[]
+  columns: ColumnDef<Data, any>[]
+}
+
+// ref: https://chakra-ui.com/getting-started/with-react-table
+export const DataTable = <Data extends object>({
+  data,
+  columns,
+}: DataTableProps<Data>) => {
+  const [sorting, setSorting] = useState<SortingState>([])
+  const table = useReactTable({
+    columns,
+    data,
+    getCoreRowModel: getCoreRowModel(),
+    onSortingChange: setSorting,
+    getSortedRowModel: getSortedRowModel(),
+    state: {
+      sorting,
+    },
+  })
+
+  return (
+    <Table>
+      <Thead>
+        {table.getHeaderGroups().map((headerGroup) => (
+          <Tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => {
+              // see https://tanstack.com/table/v8/docs/api/core/column-def#meta to type this correctly
+              const meta: any = header.column.columnDef.meta
+              return (
+                <Th
+                  key={header.id}
+                  onClick={header.column.getToggleSortingHandler()}
+                  isNumeric={meta?.isNumeric}
+                >
+                  {flexRender(
+                    header.column.columnDef.header,
+                    header.getContext(),
+                  )}
+
+                  <chakra.span pl="4">
+                    {header.column.getIsSorted() ? (
+                      header.column.getIsSorted() === 'desc' ? (
+                        <TriangleDownIcon aria-label="sorted descending" />
+                      ) : (
+                        <TriangleUpIcon aria-label="sorted ascending" />
+                      )
+                    ) : header.column.getCanSort() ? (
+                      <UpDownIcon aria-label="not sorted" />
+                    ) : null}
+                  </chakra.span>
+                </Th>
+              )
+            })}
+          </Tr>
+        ))}
+      </Thead>
+      <Tbody>
+        {table.getRowModel().rows.map((row) => (
+          <Tr key={row.id}>
+            {row.getVisibleCells().map((cell) => {
+              // see https://tanstack.com/table/v8/docs/api/core/column-def#meta to type this correctly
+              const meta: any = cell.column.columnDef.meta
+              return (
+                <Td key={cell.id} isNumeric={meta?.isNumeric}>
+                  {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                </Td>
+              )
+            })}
+          </Tr>
+        ))}
+      </Tbody>
+    </Table>
+  )
+}
--- packages/ui/index.tsx
+++ packages/ui/index.tsx
@@ -2,5 +2,6 @@ import * as React from 'react'
 
 // component exports
 export * from './Button'
+export * from './DataTable'
 export * from './Header'
 export * from './Sidebar'
