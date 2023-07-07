4ac0e2a0c0a09aea69bba529eca6674d2f6c74d2
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-07-07 18:15:52 +0900

    add table

---
+++ apps/web/src/app/[mode]/[organizationName]/prs/components/Table.tsx
@@ -0,0 +1,64 @@
+'use client'
+import 'client-only'
+
+import React, { FC } from 'react'
+import { DataTable } from '@g-dash/ui'
+import { PRsAccordion } from './PRsAccordion'
+import { Avatar, Box, Stack } from '@chakra-ui/react'
+import { createColumnHelper } from '@tanstack/react-table'
+
+type UserWithPRs = {
+  id: string
+  login: string
+  avatarUrl: string
+  Prs: {
+    id: string
+    organizationId: string
+    authorId: string
+    title: string
+    url: string
+    additions: number
+    deletions: number
+    changedFiles: number
+    commentsTotalCount: number
+    commitsTotalCount: number
+    merged: boolean
+    closed: boolean
+    createdAt: Date
+    closedAt: Date
+    mergedAt: Date
+  }[]
+}
+
+type Props = {
+  usersWithPRs: UserWithPRs[]
+}
+
+const columnHelper = createColumnHelper<UserWithPRs>()
+
+const columns = [
+  columnHelper.display({
+    id: 'Avatar',
+    cell: (props) => (
+      <Stack alignItems="center" w={32}>
+        <Avatar src={props.row.original.avatarUrl} />
+        <Box pt={1}> {props.row.original.login}</Box>
+      </Stack>
+    ),
+  }),
+  columnHelper.accessor('Prs', {
+    header: 'PRs',
+    cell: (props) => props.row.original.Prs.length,
+    meta: {
+      isNumeric: true,
+    },
+  }),
+  columnHelper.display({
+    header: '詳細',
+    cell: (props) => <PRsAccordion prs={props.row.original.Prs} />,
+  }),
+]
+
+export const Table: FC<Props> = ({ usersWithPRs }) => (
+  <DataTable columns={columns} data={usersWithPRs} />
+)
