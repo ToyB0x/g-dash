4777dc805700f22de12474507e3dab51d1656529
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-07-08 14:00:23 +0900

    add constant pakcage

---
+++ packages/constants/src/index.ts
@@ -0,0 +1,8 @@
+// export const Periods = {
+//   halfYear: new Date(Date.now() - 60 * 60 * 24 * 30 * 6 * 1000),
+//   quoter: new Date(Date.now() - 60 * 60 * 24 * 30 * 3 * 1000),
+//   month: new Date(Date.now() - 60 * 60 * 24 * 30 * 1000),
+//   week: new Date(Date.now() - 60 * 60 * 24 * 7 * 1000),
+// } as const
+
+export const Spans = [1, 2, 3, 4, 5, 6] as const
