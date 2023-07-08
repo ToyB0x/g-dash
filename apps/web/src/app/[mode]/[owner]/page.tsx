4b12f4e6aa1eb03c8084eaa918f7e690b5cb7633
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-07-08 11:41:39 +0900

    update owner root

--- apps/web/src/app/[mode]/[owner]/page.tsx
+++ apps/web/src/app/[mode]/[owner]/page.tsx
@@ -2,15 +2,16 @@
 import 'client-only'
 
 import { Center } from '@chakra-ui/react'
+import { Modes } from '@g-dash/types'
 
 export default function Page({
   params,
 }: {
-  params: { mode: 'personal' | 'organization'; organizationName: string }
+  params: { mode: (typeof Modes)[keyof typeof Modes]; owner: string }
 }) {
   return (
     <Center h="100vh">
-      {params.mode} / {params.organizationName}
+      {params.mode} / {params.owner}
     </Center>
   )
 }
