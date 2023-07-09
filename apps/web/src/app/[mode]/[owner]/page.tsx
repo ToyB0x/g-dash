826ed4efb74788eb6f4dd8128667d904ff400f3e
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-07-10 08:11:15 +0900

    feat: enable line chart (#62)

--- apps/web/src/app/[mode]/[owner]/page.tsx
+++ apps/web/src/app/[mode]/[owner]/page.tsx
@@ -50,10 +50,20 @@ export default function Page({
           },
         },
         Prs: {
+          // TODO: 以下の条件追加を検討
+          // where: {
+          //   authorId: {
+          //     not: 'BOT_kgDOAbying', // renovate id
+          //   },
+          //   createdAt: {
+          //     gte: new Date(Spans['1 month']),
+          //   },
+          // },
           select: {
             id: true,
             merged: true,
             closed: true,
+            createdAt: true,
             mergedAt: true,
             Reviews: {
               select: {
@@ -67,6 +77,20 @@ export default function Page({
         Repositories: {
           select: {
             vulnerabilityAlertsTotalCount: true,
+          },
+        },
+        Reviews: {
+          where: {
+            authorId: {
+              not: 'BOT_kgDOAbying', // renovate id
+            },
+            createdAt: {
+              gte: new Date(Spans['1 month']),
+            },
+          },
+          select: {
+            id: true,
+            createdAt: true,
           },
         },
       },
@@ -98,6 +122,76 @@ export default function Page({
         (acc, cur) => acc + cur.vulnerabilityAlertsTotalCount,
         0,
       )}
+      lineChartSeries={convertToDailyCounts(
+        organization.Prs.map((pr) => pr.createdAt),
+        organization.Prs.filter((pr) => !!pr.mergedAt).map(
+          (pr) => pr.mergedAt!,
+        ),
+        organization.Reviews.map((pr) => pr.createdAt!),
+      )}
     />
   )
+}
+
+const convertToDailyCounts = (
+  openDates: Date[],
+  mergedDates: Date[],
+  reviewDates: Date[],
+): {
+  [dateString: string]: {
+    open: number
+    merged: number
+    review: number
+  }
+} => {
+  const obj: {
+    [dateString: string]: {
+      open: number
+      merged: number
+      review: number
+    }
+  } = {}
+
+  // init obj
+  const period = 365
+  for (let i = 0; i <= period; i++) {
+    const date = new Date()
+    date.setDate(date.getDate() - i)
+    const dateString = new Date(
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDate(),
+    ).toDateString()
+
+    obj[dateString] = {
+      open: 0,
+      merged: 0,
+      review: 0,
+    }
+  }
+
+  const updateCount = (date: Date, type: 'open' | 'merged' | 'review') => {
+    const dateString = new Date(
+      date.getFullYear(),
+      date.getMonth(),
+      date.getDate(),
+    ).toDateString()
+
+    if (obj[dateString]) {
+      obj[dateString][type]++
+    } else {
+      obj[dateString] = {
+        open: 0,
+        merged: 0,
+        review: 0,
+      }
+      obj[dateString][type]++
+    }
+  }
+
+  openDates.forEach((date) => updateCount(date, 'open'))
+  mergedDates.forEach((date) => updateCount(date, 'merged'))
+  reviewDates.forEach((date) => updateCount(date, 'review'))
+
+  return obj
 }
