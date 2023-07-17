e58eeb42dcce2f35cc0c1f930edd8a7f95a544db
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-07-17 12:43:14 +0900

    remove commitsTotalCount

---
+++ packages/database/prisma/migrations/20230717034248_remove_commits_total_count/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `commitsTotalCount` on the `Pr` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Pr" DROP COLUMN "commitsTotalCount";
