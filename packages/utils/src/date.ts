d38c4b975e5473185a5b972c9fc32aa9f13fc287
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-08-11 12:22:27 +0900

    add span

--- packages/utils/src/date.ts
+++ packages/utils/src/date.ts
@@ -6,6 +6,7 @@ export const Spans = {
   '1 week': subWeeks(today, 1).getTime(),
   '2 week': subWeeks(today, 2).getTime(),
   '1 month': subMonths(today, 1).getTime(),
+  '2 month': subMonths(today, 2).getTime(),
   '3 month': subMonths(today, 3).getTime(),
   '6 month': subMonths(today, 6).getTime(),
   'start of month': startOfMonth(today).getTime(),
