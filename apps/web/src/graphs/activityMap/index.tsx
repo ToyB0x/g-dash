c316e4335db228429b7be12f2d527cae90b899bc
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-08-12 14:17:15 +0900

    update index

--- apps/web/src/graphs/activityMap/index.tsx
+++ apps/web/src/graphs/activityMap/index.tsx
@@ -3,6 +3,7 @@ import 'server-only'
 import { FC, Suspense } from 'react'
 import { Skeleton } from '@g-dash/ui'
 import Container from './Container'
+import { GraphArgs } from '@/graphs'
 
 const ContainerWithSuspense: FC<GraphArgs> = ({
   orgId,
--- apps/web/src/graphs/index.ts
+++ apps/web/src/graphs/index.ts
@@ -1,2 +1,1 @@
-export * from './activityMap'
 export * from './interface'
