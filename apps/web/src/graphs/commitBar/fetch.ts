c7edee534b5d3e17a0112a079e63b9c65cf8b8b0
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-08-13 10:57:58 +0900

    add commit bar fetcher

---
+++ apps/web/src/graphs/commitBar/fetch.ts
@@ -0,0 +1,45 @@
+import { getSingleTenantPrismaClient } from '@/clients'
+
+export const fetch = async (
+  orgId: string,
+  userIds: string[],
+  startDate: Date,
+) => {
+  const prisma = getSingleTenantPrismaClient()
+  const organization = await prisma.organization.findUniqueOrThrow({
+    where: {
+      login: orgId,
+    },
+    select: {
+      id: true,
+      Commits: {
+        where: {
+          committedDate: {
+            gte: startDate,
+          },
+          user: {
+            login: {
+              in: userIds.length ? userIds : undefined,
+            },
+          },
+        },
+        select: {
+          id: true,
+          committedDate: true,
+          user: {
+            select: {
+              login: true,
+            },
+          },
+        },
+      },
+    },
+  })
+
+  return {
+    barChartSeries: organization.Commits.map((c) => ({
+      login: c.user.login,
+      committedDate: c.committedDate,
+    })),
+  }
+}
