ac0c38aef693d4aeaae74d2ac28733ec02f92d9a
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-08-13 09:41:14 +0900

    add import

--- apps/web/src/graphs/activityMap/Container.tsx
+++ apps/web/src/graphs/activityMap/Container.tsx
@@ -3,6 +3,7 @@ import 'server-only'
 import { FC } from 'react'
 import { UI } from './UI'
 import { fetch } from './fetch'
+import { GraphArgs } from '@/graphs'
 
 const Container: FC<GraphArgs> = async ({ orgId, userIds, startDate }) => {
   const { committedDates, reviewedDates } = await fetch(
