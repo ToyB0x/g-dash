1b071c0ae7d56b94ec7ebdc5747b866dc5488282
ToyB0x <83337042+ToyB0x@users.noreply.github.com>
2023-08-13 10:58:11 +0900

    add commit bar container

---
+++ apps/web/src/graphs/commitBar/Container.tsx
@@ -0,0 +1,14 @@
+import 'server-only'
+
+import { FC } from 'react'
+import { UI } from './UI'
+import { fetch } from './fetch'
+import { GraphArgs } from '@/graphs'
+
+const Container: FC<GraphArgs> = async ({ orgId, userIds, startDate }) => {
+  const { barChartSeries } = await fetch(orgId, userIds, startDate)
+
+  return <UI barChartSeries={barChartSeries} />
+}
+
+export default Container
